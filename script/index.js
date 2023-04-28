8e587240fd8e68b266f9fc436b21bbf73a678198
Ssafar2 <sahib.safarali22@gmail.com>
2023-04-28 21:06:41 +0400

    Menyu modal və Təşəkkür

--- script/index.js
+++ script/index.js
@@ -1,7 +1,84 @@
 const faBars = document.querySelector('#faBars');
 const menuUl = document.querySelector('.menuUl');
-const faSun = document.querySelector('#faSun')
+const faSun = document.querySelector('#faSun');
 const docBody = document.body;
+const menuImg = document.querySelectorAll('.menu-img');
+const menuImgModule = document.querySelector('#menu-img-module');
+const closeModal=document.querySelector('#close-modal')
+
+
+
+
+
+
+
+const menyu = [
+    {
+        title: 'Portağal şirəsi',
+        content: 'Ləzzətli İçəcəklər burada',
+        image:'./image/drink1.jpg'
+    },
+    {
+        title: 'Hamburer',
+        content: 'Ləzzətli Hamburgerlər burada',
+        image:'./image/hamburger1.jpg'
+    },
+    {
+        title: 'Toyuq qızartması',
+        content: 'Ləzzətli Toyuq qızartmaları burada',
+        image:'./image/chicken.jpg'
+    },
+    {
+        title: 'Pizza',
+        content: 'Ləzzətli Pizzalar burada',
+        image:'./image/pizza1.jpg'
+    },
+    {
+        title: 'Salat',
+        content: 'Ləzzətli Salatlar burada',
+        image:'./image/salad.jpg'
+    },
+    {
+        title: 'Sandwich',
+        content: 'Ləzzətli Sandwichlər burada',
+        image:'./image/sandwich.jpg'
+    },
+    {
+        title: 'Buzlu limon suyu',
+        content: 'Ləzzətli İçəcəklər burada',
+        image:'./image/drink3.jpg'
+    },
+    {
+        title: 'Hotdog',
+        content: 'Ləzzətli Hotdoglar burada',
+        image:'./image/hotdog1.jpg'
+    },
+]
+
+
+
+
+
+
+menuImg.forEach((value, index) => {
+
+    value.addEventListener('click', () => {
+        menuImgModule.style.display = 'block'
+
+        menyu.map((val,ind)=>{
+            if(index===ind){
+                console.log('hi');
+                menuImgModule.querySelector('img').src=val.image;
+                menuImgModule.querySelector('h1').textContent=val.title;
+                menuImgModule.querySelector('p').textContent=val.content;
+            }
+        })
+    })
+})
+
+closeModal.addEventListener('click',()=>{
+    menuImgModule.style.display='none'
+})
 
 
 
@@ -26,11 +103,11 @@ faSun.addEventListener('click', () => {
 
     if (docBody.classList.contains('lightMode')) {
         faSun.classList.replace('fa-sun', 'fa-moon')
-    
+
     }
     else {
         faSun.classList.replace('fa-moon', 'fa-sun')
-     
+
     }
 
 
